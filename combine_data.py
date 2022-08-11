dd42e22cd590e22ecaec1f0853f390e582bd639a
Seth16225 <79070283+Seth16225@users.noreply.github.com>
2022-08-11 11:49:36 -0500

    modify combine_data

--- combine_data.py
+++ combine_data.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import pm25
 import numpy as np
 import pandas as pd
@@ -37,52 +37,3 @@ for index, row in coord_list.iterrows():
 
         if(distance >= dst(row, ))
         distance = dst(row)
-       
-    
-    
-
-
-
-#combined_df = pd.merge(pm25_df,pop_df)
-=======
-import pm25
-import numpy as np
-import pandas as pd
-import os
-import georasters as gr
-import geopy as gp
-
-#import data from Tif file and convert to dataframe
-popdens = gr.from_file('variables\gpw_v4_population_density_rev11_2020_2pt5_min.tif')
-pop_df = popdens.to_pandas()
-pop_df = pop_df.rename(columns={"x": "Longitude", "y": "Latitude", "value": "pop_density"})
-
-#remove unnecessary column and row variables from the dataframe
-pop_df.drop(columns=['row', 'col'],inplace=True)
-
-#convert pop_df into array.
-pop_ar = pop_df.to_numpy()
-pop_ar = np.delete(pop_ar, 0, axis=1)
-
-#pm2.5 data inport
-pm25_df = pd.read_csv("pm25/AQS_US_PM2520210101_20210131.csv")
-
-
-
-#creates a longitude and latitude dataframe, then gets unique coordinate pair values from pm25, then associates it with data.
-coord_list = pd.DataFrame(columns=["Longitude", "Latitude"])
-
-coord_list["Longitude"] = pm25_df["Longitude"].unique()
-coord_list["Latitude"] = pm25_df["Latitude"].unique()
-
-for row in pop_ar:
-    print(row)
-
-#for index, row in coord_list.iterrows():
-    
-
-
-
-#combined_df = pd.merge(pm25_df,pop_df)
->>>>>>> cca35aef36eeb90d35e9443f2d3e497de81d71f3
-#combined_df.to_csv(os.path.join("outputs",'combined_dataframe' + '.csv'))
