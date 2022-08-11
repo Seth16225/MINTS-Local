959ecf63c7fcf2a5f6e06170fe16c8256cec923b
Seth16225 <79070283+Seth16225@users.noreply.github.com>
2022-08-11 11:46:25 -0500

    combine_data added

--- pm25.py
+++ pm25.py
@@ -3,9 +3,9 @@ import requests
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-from cartopy.feature import ShapelyFeature
-from cartopy.io.shapereader import Reader
+#import cartopy.crs as ccrs
+#from cartopy.feature import ShapelyFeature
+#from cartopy.io.shapereader import Reader
 
 statelist = ['01','04','05','06','07','08','09','10','11','12','13','16','17','18','19','20',
 '21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39',
